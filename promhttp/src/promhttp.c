b46b551700e896e03059647beccaf035cf79369e
Molly Miller <molly.miller@wire.com>
2022-03-16 10:51:15 +0100

    Add compatibility with newer libmicrohttpd access handler types.
    Starting with libmicrohttpd 0.9.71, the access handler callback function
    type has changed to return an `enum MHD_Result` instead of an `int`, so the
    return type of promhttp_handler needs to be corrected based on the version of
    libmicrohttpd it's compiled against.
    
    Additionally, the visibility of this function is changed to static, as it's not
    exposed by the promhttp.h header file.

--- promhttp/src/promhttp.c
+++ promhttp/src/promhttp.c
@@ -19,6 +19,12 @@
 #include "microhttpd.h"
 #include "prom.h"
 
+#if MHD_VERSION >= 0x00097002
+typedef enum MHD_Result AccessHandlerResult;
+#else
+typedef int AccessHandlerResult;
+#endif
+
 prom_collector_registry_t *PROM_ACTIVE_REGISTRY;
 
 void promhttp_set_active_collector_registry(prom_collector_registry_t *active_registry) {
@@ -29,32 +35,32 @@ void promhttp_set_active_collector_registry(prom_collector_registry_t *active_re
   }
 }
 
-int promhttp_handler(void *cls, struct MHD_Connection *connection, const char *url, const char *method,
+static AccessHandlerResult promhttp_handler(void *cls, struct MHD_Connection *connection, const char *url, const char *method,
                      const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls) {
   if (strcmp(method, "GET") != 0) {
     char *buf = "Invalid HTTP Method\n";
     struct MHD_Response *response = MHD_create_response_from_buffer(strlen(buf), (void *)buf, MHD_RESPMEM_PERSISTENT);
-    int ret = MHD_queue_response(connection, MHD_HTTP_BAD_REQUEST, response);
+    AccessHandlerResult ret = MHD_queue_response(connection, MHD_HTTP_BAD_REQUEST, response);
     MHD_destroy_response(response);
     return ret;
   }
   if (strcmp(url, "/") == 0) {
     char *buf = "OK\n";
     struct MHD_Response *response = MHD_create_response_from_buffer(strlen(buf), (void *)buf, MHD_RESPMEM_PERSISTENT);
-    int ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+    AccessHandlerResult ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
     MHD_destroy_response(response);
     return ret;
   }
   if (strcmp(url, "/metrics") == 0) {
     const char *buf = prom_collector_registry_bridge(PROM_ACTIVE_REGISTRY);
     struct MHD_Response *response = MHD_create_response_from_buffer(strlen(buf), (void *)buf, MHD_RESPMEM_MUST_FREE);
-    int ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+    AccessHandlerResult ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
     MHD_destroy_response(response);
     return ret;
   }
   char *buf = "Bad Request\n";
   struct MHD_Response *response = MHD_create_response_from_buffer(strlen(buf), (void *)buf, MHD_RESPMEM_PERSISTENT);
-  int ret = MHD_queue_response(connection, MHD_HTTP_BAD_REQUEST, response);
+  AccessHandlerResult ret = MHD_queue_response(connection, MHD_HTTP_BAD_REQUEST, response);
   MHD_destroy_response(response);
   return ret;
 }
